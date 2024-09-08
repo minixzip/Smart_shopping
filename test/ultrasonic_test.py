de95abe9f9fa8c7292c4af33450e72fcf0be2015
Khung <simyeyeong657@gmail.com>
2024-09-08 21:27:06 +0900

    test module

--- test/ultrasonic_test.py
+++ test/ultrasonic_test.py
@@ -1,26 +1,48 @@
-#!/usr/bin/env pyhton
+#!/usr/bin/env python
 import RPi.GPIO as GPIO
+import time
 import time
 
 GPIO.setmode(GPIO.BCM)
+GPIO.setwarnings(False)
 
 # 초음파 센서 GPIO 핀 설정
 TRIG_PINS = [2, 5, 7]
 ECHO_PINS = [3, 6, 8]
+result = [500,500,500]
+print('초음파 거리 측정')
 
-for TRIG, ECHO in zip(TRIG_PINS, ECHO_PINS):
-    GPIO.setup(TRIG, GPIO.OUT)
-    GPIO.setup(ECHO, GPIO.IN)
+for i in range(3):
+    GPIO.setup(TRIG_PINS[i], GPIO.OUT)
+    GPIO.setup(ECHO_PINS[i], GPIO.IN)
+    GPIO.output(TRIG_PINS[i], GPIO.LOW)
 
-def get_distance(trig_pin, echo_pin):
-    GPIO.output(trig_pin, True)
+def get_distance(num):
+    GPIO.output(TRIG_PINS[num], False)
+    print('초음파 출력 초기화')
+    time.sleep(0.01)
+    GPIO.output(TRIG_PINS[num], True)
     time.sleep(0.00001)
-    GPIO.output(trig_pin, False)
+    GPIO.output(TRIG_PINS[num], False)
     
-    while GPIO.input(echo_pin) == 0:
+    while GPIO.input(ECHO_PINS[num]) == 0:
         start_time = time.time()
     
-    while GPIO.input(echo_pin) == 1:
+    while GPIO.input(ECHO_PINS[num]) == 1:
         stop_time = time.time()
     
     time_elapsed = stop_time - start_time
+    dis = round(time_elapsed*17000,2)
+    
+try:
+    while True:
+        for i in range(3):
+            result[i] = get_distance(i)
+            
+            if result[i]<30 and result[i] >=3:
+                print(f'센서{i+1} 거리는 {result[i]}cm')
+        
+        
+except KeyboardInterrupt:
+    print('거리 측정 완료')
+    GPIO.cleanup()
--- test/weightsensor_test.py
+++ test/weightsensor_test.py
@@ -1,26 +1,91 @@
-#!/usr/bin/env pyhton
+#!/usr/bin/env python
 import RPi.GPIO as GPIO
-from hx711 import HX711
+import time
 
 # 무게 센서 GPIO 핀 설정
-DT_PIN = 17
-SCK_PIN = 27
+DT = 17
+SCK = 27
+sample=0
+pre_gram=0
 
-hx = HX711(DT_PIN, SCK_PIN)
-hx.set_reading_format("MSB", "MSB")
-hx.set_reference_unit(1)
-hx.reset()
-hx.tare()
+def get_weight():
+    i=0
+    Count=0
+    GPIO.setup(DT,GPIO.OUT)
+    GPIO.output(DT,1)
+    GPIO.output(SCK,0)
+    GPIO.setup(DT,GPIO.IN)
+    while GPIO.input(DT)==1:
+        i=0
+    for i in range(24):
+        GPIO.output(SCK,1)
+        Count=Count<<1
+        GPIO.output(SCK,0)
+        if GPIO.input(DT)==0:
+            Count=Count+1
+    GPIO.output(SCK,1)
+    Count=Count^0x800000
+    GPIO.output(SCK,0)
+    
+    return Count
 
-print("Tare done! Add weight now...")
+def send_weight():
+    global pre_gram    
+    w=0
+    result_gram=0
+    count=[]
+    plus=[]
+    minus=[]
+    equal=[]
+    for each in range(3):
+        count.append(get_weight())
+        time.sleep(0.5)
+    
+    cntArr={"+":0, "-":0, "=":0}
+    
+    for i in range(len(count)):
+        w=((sample-count[i])/106)
+    
+#        print('round gram',round(w))
+        gram=max(0,round(w))
+#        print('gram[',i+1,']',gram,'g')
+        count[i] = gram
+        result_gram=gram-pre_gram
+#        print('gram[',i+1,']',result_gram,'error g')
+        if result_gram>80:
+            cntArr['+'] += 1
+            plus.append(count[i])
+        elif result_gram<-80:
+            cntArr['-'] += 1
+            minus.append(count[i])
+        else:
+            cntArr['='] += 1
+            equal.append(count[i])
+    
+    Max = cntArr["+"]
+    MaxK = "+"
+    for (eachK,eachV) in cntArr.items():
+        if eachV > Max:
+            Max = eachV
+            MaxK = eachK
+            
+
+    if MaxK=="+": #plus min
+        pre_gram = min(plus)
+    elif MaxK =="-":# minus max
+        pre_gram = max(minus)
+    else:#equal min
+        pre_gram = min(equal)
+    print(MaxK)
+    return MaxK
+
+print("무게 감지 시작")
 
 try:
     while True:
-        val = hx.get_weight(5)
-        print(f"Weight: {val} g")
-        hx.power_down()
-        hx.power_up()
+        GPIO.setup(SCK,GPIO.OUT)
+        print(f"Weight: {send_weight()}")
         time.sleep(1)
 except KeyboardInterrupt:
-    print("Measurement stopped by User")
+    print("무게 센서 종료")
     GPIO.cleanup()
